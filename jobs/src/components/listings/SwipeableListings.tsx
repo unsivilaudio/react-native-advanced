f95c113658c13b2bf1f3893ac0144042e0782686
unsivil <unsivilaudio@gmail.com>
2024-04-08 11:21:05 -0600

    little bugfix

--- jobs/src/components/listings/SwipeableListings.tsx
+++ jobs/src/components/listings/SwipeableListings.tsx
@@ -1,10 +1,13 @@
-import ListItem from '@/components/listings/ListItem';
-import Swipe from '@/components/ui/Swipe';
-import { Job } from '@/providers/jooble';
+import { Text, View, StyleSheet } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+
+import type { Job } from '@/providers/jooble';
 import { useAuthStore } from '@/store/hooks/use-auth';
 import { useJobsStore } from '@/store/hooks/use-jobs';
-import { useFocusEffect } from '@react-navigation/native';
-import { View, StyleSheet } from 'react-native';
+
+import Card from '@/components/ui/Card';
+import ListItem from '@/components/listings/ListItem';
+import Swipe from '@/components/ui/Swipe';
 
 type SwipeableListingsProp = {
     jobs: Job[];
@@ -20,12 +23,27 @@ export default function SwipeableListings({ jobs }: SwipeableListingsProp) {
         }
     }
 
+    function handleRenderNoMoreJobs() {
+        return (
+            <View style={[styles.container, { justifyContent: 'center' }]}>
+                <Card style={styles.noMoreJobsContainer}>
+                    <Ionicons name='alert-circle' size={45} />
+                    <Text style={styles.noMoreJobsText}>
+                        No more jobs to show you...navigate back to the map to
+                        get more results.
+                    </Text>
+                </Card>
+            </View>
+        );
+    }
+
     return (
         <View style={styles.container}>
             <Swipe
                 data={jobs}
                 renderCard={item => <ListItem key={item.id} {...item} />}
                 onSwipeRight={handleSwipeRight}
+                renderNoMoreCards={handleRenderNoMoreJobs}
             />
         </View>
     );
@@ -35,5 +53,19 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         padding: 20,
+    },
+    noMoreJobsContainer: {
+        flex: 0,
+        height: undefined,
+        padding: 20,
+        paddingHorizontal: 30,
+        justifyContent: 'center',
+        alignItems: 'center',
+        gap: 20,
+    },
+    noMoreJobsText: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        textAlign: 'center',
     },
 });
